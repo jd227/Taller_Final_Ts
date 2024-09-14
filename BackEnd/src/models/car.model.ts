a8cdf0cb29a42aafbecf52e4f9166adb377cc966
jd227 <jd821jd@gmail.com>
2024-09-13 21:54:00 -0500

    configuracion de la api, y consumo de la api en el index del front

--- BackEnd/src/models/car.model.ts
+++ BackEnd/src/models/car.model.ts
@@ -4,7 +4,9 @@ const carSchema = new Schema({
   brand: { type: String, required: true },
   model: { type: String, required: true },
   year: { type: Number, required: true },
-  owner: { type: Schema.Types.ObjectId, ref: 'User' } // Relación con el modelo de usuario
+  owner: { type: String, required: true}, // Relación con el modelo de usuario
+  Image: { type: String, required: true},
+  price: { type: Number, required: true}
 });
 
 export const Car = model('Car', carSchema);
