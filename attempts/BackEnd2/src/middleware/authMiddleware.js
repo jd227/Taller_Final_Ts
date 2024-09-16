5249bb17329ea53eaa0a3eaba2f3bae747cedb0e
jd227 <jd821jd@gmail.com>
2024-09-16 16:42:46 -0500

    Creacion y funcionalidad del formulario de edición

--- attempts/BackEnd2/src/middleware/authMiddleware.js
+++
@@ -1,7 +0,0 @@
-export const isAuthenticated = (req, res, next) => {
-  if (req.session.user) {
-    next(); // Si la sesión está activa, continúa
-  } else {
-    res.status(401).json({ message: 'No autenticado' }); // Si no hay sesión, devuelve un error
-  }
-};
